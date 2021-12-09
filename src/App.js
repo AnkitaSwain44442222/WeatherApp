7d4e27d2b318e5dbb43c5efbd360bcf8ef0e9c9f
AnkitaSwain44442222 <79967597+AnkitaSwain44442222@users.noreply.github.com>
2021-12-09 19:16:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+//import Todo from './component/todoreact/todo'
+import Temp from "./component/weather/temp"
+
+const App = () => {
+  // return <UseEffect/>;
+  return (
+  <>
+    <Temp/>
+  </>
+  )}
+
+export default App
